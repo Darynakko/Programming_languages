d46380f568fbc5a150b6eb2146af4f127bd78b8a
Daryna <danako42@gmail.com>
2024-12-04 13:40:44 +0000

    changes

--- routes/main.js
+++ routes/main.js
@@ -72,7 +72,38 @@ router.get('/register', function (req,res) {
 
 router.post('/registered', function (req,res) {
     // saving data in database
-    res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email);                                                                              
+    let sqlquery = "INSERT INTO users (name, email, password) VALUES (?,?,?)";
+    // execute sql query
+    let newrecord = [req.body.name, req.body.email, req.body.password];
+    db.query(sqlquery, newrecord, (err, result) => {
+    if (err) {
+        return console.error(err.message);
+        }
+    else {
+        res.send(' Hello '+ req.body.name + ' you are now registered!  We will send you an email at ' + req.body.email + ' <p><a href="/">Go back to the main page</a></p>');
+        }
+    });
+});
+
+router.get('/login', function (req, res) {
+    res.render('login.ejs', appData);                                                                     
+});      
+
+router.post('/loggedin', function (req, res) {
+    // saving data in database
+    const email = req.body.email;
+    const password = req.body.password;
+    
+    let sqlquery = "SELECT * FROM users WHERE email = ? AND password = ?";
+    // execute sql query
+    db.query(sqlquery, [email, password], (err, result) => {
+    if (err) {
+        return console.error(err.message);
+        }
+    else {
+        res.send(' Hello '+ result[0].name + '!  Successful login ' + ' <p><a href="/">Go back to the main page</a></p>');
+        }
+    });
 });
 
 // Export the router object so index.js can access it
