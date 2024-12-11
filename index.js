20dc870c6f80141e8c4a5161575c6d400ddceb65
Daryna <danako42@gmail.com>
2024-12-11 16:58:27 +0000

    changes

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var express = require ('express');
 var ejs = require('ejs');
 var mysql = require('mysql2');
 
+const session = require('express-session');
+const path = require('path');
+
 // Create the express application object
 const app = express()
 const port = 8000
@@ -13,6 +16,13 @@ app.use(express.urlencoded({ extended: true }));
 // Set up css
 app.use(express.static(__dirname + '/styles'));
 
+//session data for login
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(express.json());
 
 // Tell Express that we want to use EJS as the templating engine
 app.set('view engine', 'ejs');
--- routes/main.js
+++ routes/main.js
@@ -5,6 +5,7 @@ const router = express.Router();
 // Define our data
 var appData = {appName: "Programming library"}
 
+
 // Handle our routes
 router.get('/',function(req,res){
     res.render('index.ejs', appData)
@@ -58,12 +59,30 @@ router.get('/cplusplus', function(req, res) {
 });
 
 router.get('/search',function(req,res){
-    res.render("search.ejs", appData);
+    res.render("search.ejs", { appName: "Programming Search" });
 });
 
 router.get('/search-result', function (req, res) {
     //searching in the database
-    res.send("You searched for: " + req.query.keyword);
+    const keyword = req.query.keyword;
+
+    const sqlquery = `
+     SELECT 'cplusplus' AS source, id, name, link FROM cplusplus WHERE name LIKE ?
+     UNION
+     SELECT 'python' AS source, id, name, link FROM python WHERE name LIKE ?
+     UNION
+     SELECT 'javascript' AS source, id, name, link FROM javascript WHERE name LIKE ?
+     `;
+
+    // let sqlquery = " SELECT * FROM [SearchTable]  WHERE [availableBooks] LIKE % key % ";
+    db.query(sqlquery,[`%${keyword}%`, `%${keyword}%`,`%${keyword}%`] ,(err, result) => {
+        if (err) {
+            console.error("Database error:", err);
+            res.redirect('./'); 
+            return;
+        }
+        res.render("search_result.ejs", { results: result, keyword: keyword });
+     });
 });
 
 router.get('/register', function (req,res) {
@@ -80,7 +99,7 @@ router.post('/registered', function (req,res) {
         return console.error(err.message);
         }
     else {
-        res.send(' Hello '+ req.body.name + ' you are now registered!  We will send you an email at ' + req.body.email + ' <p><a href="/">Go back to the main page</a></p>');
+        res.send(' Thank you, '+ req.body.name + ' you are now registered!  We will send you an email at ' + req.body.email + ' <p><a href="/">Go back to the main page</a></p>');
         }
     });
 });
@@ -89,38 +108,66 @@ router.get('/login', function (req, res) {
     res.render('login.ejs', appData);                                                                     
 });      
 
-router.post('/loggedin', function (req, res) {
-    // saving data in database
+router.get('/loggedin', function (req, res) {
     const email = req.body.email;
     const password = req.body.password;
     
     let sqlquery = "SELECT * FROM users WHERE email = ? AND password = ?";
     // execute sql query
     db.query(sqlquery, [email, password], (err, result) => {
-    if (err) {
-        return console.error(err.message);
-        }
-    else {
-        res.send(' Hello '+ result[0].name + '!  Successful login ' + ' <p><a href="/">Go back to the main page</a></p>');
-        }
-    });
+    // if (err) {
+    //     console.error(e rr.message);
+    //     res.redirect('/login');
+    //     return;
+    //     }
+    // if (result.length > 0) {
+    //     // Store user info in session
+    //     req.session.user = result[0];
+    //     res.send(' Hello '+ result[0].name + '!  Successful login ' + ' <p><a href="/">Go back to the main page</a></p>' + ' <p><a href="/dashboard">See dashboard</a></p>');
+    // } else {
+    //    res.send("Invalid username or password");
+    // }
+    // });
+    
+        if (req.session.loggedin) {
+         // Store user info in session
+		    res.send('Welcome back, ' + req.session.name  + '! Successful login ' + ' <p><a href="/">Go back to the main page</a></p> ');
+	    } else {
+		// Not logged in
+		    res.send('Please login to view this page!');
+    	}
+    	res.end();
+});
 });
 
-router.post('/update-book-status', (req, res) => {
-    const { bookId, isRead } = req.body.name;
-  
-    if (!bookId || isRead === undefined) {
-      return res.status(400).send('Invalid request');
-    }
-    const query = `UPDATE books SET is_read = ? WHERE id = ?`;
-    db.query(query, [isRead ? 1 : 0, bookId], (err) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).send('Database update failed');
-      }
-      res.send('Success');
-    });
-  });
+router.post('/auth', function(req, res) {
+	// Capture the input fields
+	let name = req.body.name;
+	let password = req.body.password;
+	// Ensure the input fields exists and are not empty
+	if (name && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		db.query('SELECT * FROM users WHERE name = ? AND password = ?', [name, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				req.session.loggedin = true;
+				req.session.name = name;
+				// Redirect to home page
+				res.redirect('/loggedin');
+			} else {
+				res.send('Incorrect Username and/or Password!');
+			}			
+			res.end();
+		});
+	} else {
+		res.send('Please enter Username and Password!');
+		res.end();
+	}
+});
+
 
 // Export the router object so index.js can access it
 module.exports = router;
